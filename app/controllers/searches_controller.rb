eb641054bff53d6a8f27f56aada411935909da05
Samantha Morco <samanthamorco@gmail.com>
2017-06-19 13:56:34 -0400

    add search box

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,8 @@ class SearchesController < ApplicationController
 
   def index
     @borough = params[:borough]
-    @search_results = HealthCenter.where(borough: params[:borough])
+    if params[:search] && params[:search].downcase.include?("health") && params[:search].downcase.include?("center")
+      @search_results = HealthCenter.where(borough: params[:borough])
+    end
   end
 end
